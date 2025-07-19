4b80ef320b22c5010ee476360cfb10ecfb8c2988
kem198 <kenkenpa198@gmail.com>
2025-07-19 22:27:06 +0900

    Add initial implementation of TodosCoreApplication and its tests

--- src/api/todos-core/src/main/java/net/kem198/todos_core/TodosCoreApplication.java
+++ src/api/todos-core/src/main/java/net/kem198/todos_core/TodosCoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodosApiApplication {
+public class TodosCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodosApiApplication.class, args);
+        SpringApplication.run(TodosCoreApplication.class, args);
     }
 
 }
