b4c7dcc4f8ec075190d4cd1ebf8baf6ab8fa25ec
KeM198 <kenkenpa198@gmail.com>
2025-07-25 00:09:50 +0900

    refactor: remove TodosInfrastructureApplication and its tests; introduce TodosInfrastructureTestApplication for better structure

---
+++ src/backend/todos-infrastructure/src/test/java/net/kem198/todos/infrastructure/TodosInfrastructureTestApplication.java
@@ -0,0 +1,13 @@
+package net.kem198.todos.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "net.kem198.todos")
+public class TodosInfrastructureTestApplication {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
