94c9ebc477a4751ff8babbea9c3559766d9491a0
kem198 <kenkenpa198@gmail.com>
2025-07-24 20:41:47 +0900

    refactor: clean up comments and format annotation in TodosInfrastructureApplication

--- src/backend/todos-infrastructure/src/main/java/net/kem198/todos/infrastructure/TodosInfrastructureApplication.java
+++ src/backend/todos-infrastructure/src/main/java/net/kem198/todos/infrastructure/TodosInfrastructureApplication.java
@@ -3,7 +3,9 @@ package net.kem198.todos.infrastructure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "net.kem198.todos" })
+// HACK: infrastructure 層にはエントリポイントは必要ないが、テスト実行のため残している
+// できれば消したい
+@SpringBootApplication(scanBasePackages = "net.kem198.todos")
 public class TodosInfrastructureApplication {
 
     public static void main(String[] args) {
