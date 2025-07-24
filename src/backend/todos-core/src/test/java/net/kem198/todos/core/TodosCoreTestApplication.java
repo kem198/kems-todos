1b1a924451f6dd8001f279c7b16d2e27df5f4648
KeM198 <kenkenpa198@gmail.com>
2025-07-25 00:09:44 +0900

    fix: specify scanBasePackages in @SpringBootApplication annotation for TodosCoreTestApplication

--- src/backend/todos-core/src/test/java/net/kem198/todos/core/TodosCoreTestApplication.java
+++ src/backend/todos-core/src/test/java/net/kem198/todos/core/TodosCoreTestApplication.java
@@ -3,7 +3,7 @@ package net.kem198.todos.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "net.kem198.todos")
 public class TodosCoreTestApplication {
 
     @Test
