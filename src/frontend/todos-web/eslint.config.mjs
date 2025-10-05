b89b4c9125f64f60bf7a2a162154f8db0e9ea3e2
kem198 <kenkenpa198@gmail.com>
2025-10-05 13:06:58 +0900

    fix: enhance ESLint configuration by adding Unicorn plugin rules

--- src/frontend/todos-web/eslint.config.mjs
+++ src/frontend/todos-web/eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -23,4 +24,20 @@ const eslintConfig = [
   },
 ];
 
-export default [eslintConfig, eslintConfigPrettier];
+export default [
+  eslintConfig,
+  eslintPluginUnicorn.configs.recommended,
+  {
+    rules: {
+      ...eslintPluginUnicorn.configs.recommended.rules,
+      "unicorn/consistent-function-scoping": "off",
+      "unicorn/numeric-separators-style": "off",
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/switch-case-braces": "off",
+      "unicorn/no-static-only-class": "off",
+      "unicorn/no-useless-undefined": "off",
+    },
+  },
+  ,
+  eslintConfigPrettier,
+];
