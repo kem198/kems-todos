5047613dd6184264a8b81158b2451fad160d6b90
kem198 <kenkenpa198@gmail.com>
2025-10-13 23:01:28 +0900

    feat: add test directory and match pattern to Playwright configuration

--- src/frontend/todos-web/playwright.config.ts
+++ src/frontend/todos-web/playwright.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  testDir: "tests/e2e",
+  testMatch: "**/*.spec.ts",
   use: {
     baseURL: "http://localhost:3000",
   },
