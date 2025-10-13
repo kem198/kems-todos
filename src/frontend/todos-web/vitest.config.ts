8e7897dcb32195266b142cf40e501a1fb9f74150
kem198 <kenkenpa198@gmail.com>
2025-10-13 10:17:15 +0900

    feat: add vitest for testing and create sum utility function
    - Added vitest and vite-tsconfig-paths to devDependencies in package.json.
    - Created a sum utility function in src/utils/example/sum.ts.
    - Added a test for the sum function in tests/utils/example/sum.test.ts.
    - Created vitest configuration file vitest.config.ts.

---
+++ src/frontend/todos-web/vitest.config.ts
@@ -0,0 +1,12 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["tests/**/*.test.ts"],
+    // setupFiles: ["tests/setup.ts"],
+  },
+  plugins: [tsconfigPaths()],
+});
