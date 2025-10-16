642002c8fef64dc064d567bd7acd3548fe4c428b
KeM198 <kenkenpa198@gmail.com>
2025-10-16 20:19:36 +0900

    feat: create ShadcnuiPage component with button

---
+++ src/frontend/todos-web/src/app/example/shadcn/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function ShadcnuiPage() {
+  return (
+    <div className="m-4 flex flex-col gap-4">
+      <Button className="w-24">ボタン</Button>
+    </div>
+  );
+}
