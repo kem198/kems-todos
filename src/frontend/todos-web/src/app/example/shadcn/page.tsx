561bd9953f65d619328e1da267362d4701d4b09f
KeM198 <kenkenpa198@gmail.com>
2025-10-16 20:31:51 +0900

    feat: restructure ShadcnPage layout and add button variants

--- src/frontend/todos-web/src/app/example/shadcn/page.tsx
+++ src/frontend/todos-web/src/app/example/shadcn/page.tsx
@@ -2,8 +2,34 @@ import { Button } from "@/components/ui/button";
 
 export default function ShadcnPage() {
   return (
-    <div className="m-4 flex flex-col gap-4">
-      <Button className="w-24">ボタン</Button>
+    <div className="flex h-screen gap-4 bg-yellow-50">
+      <div className="flex w-40 flex-col gap-4 p-4">
+        <Button>default</Button>
+        <Button variant="destructive">destructive</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="link">link</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+      </div>
+
+      <div className="flex w-40 flex-col gap-4 p-4">
+        <Button size="sm">sm</Button>
+        <Button>default</Button>
+        <Button size="lg">lg</Button>
+
+        <div className="flex items-center gap-2">
+          <Button size="icon-sm"></Button>
+          <p>icon-sm</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <Button size="icon"></Button>
+          <p>icon</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <Button size="icon-lg"></Button>
+          <p>icon-lg</p>
+        </div>
+      </div>
     </div>
   );
 }
