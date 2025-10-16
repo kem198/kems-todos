e2c05f367ce1b2afb87ef59a20a61cb83e43b6c5
KeM198 <kenkenpa198@gmail.com>
2025-10-16 20:20:31 +0900

    fix: correct component name from ShadcnuiPage to ShadcnPage

--- src/frontend/todos-web/src/app/example/shadcn/page.tsx
+++ src/frontend/todos-web/src/app/example/shadcn/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function ShadcnuiPage() {
+export default function ShadcnPage() {
   return (
     <div className="m-4 flex flex-col gap-4">
       <Button className="w-24">ボタン</Button>
