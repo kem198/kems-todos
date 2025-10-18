004f4dd2f9ff619af149f8794c26c06d8ac5c9ad
kem198 <kenkenpa198@gmail.com>
2025-10-18 16:02:45 +0900

    fix: handle loading state in JsonDisplay component

--- src/frontend/todos-web/src/components/display/json-display.tsx
+++ src/frontend/todos-web/src/components/display/json-display.tsx
@@ -1,7 +1,7 @@
 export const JsonDisplay = ({ data }: { data: unknown }) => {
   return (
-    <pre className="cursor-text overflow-x-auto rounded border border-gray-500 bg-gray-50 p-2 text-xs select-text">
-      {JSON.stringify(data, undefined, 2)}
+    <pre className="w-full cursor-text overflow-x-auto rounded border border-gray-500 bg-gray-50 p-2 text-xs select-text">
+      {data ? JSON.stringify(data, undefined, 2) : "Loading..."}
     </pre>
   );
 };
