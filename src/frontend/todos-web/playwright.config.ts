f7d9b4e984ec4566d9cbd4b99b0160b670dc5969
KeM198 <kenkenpa198@gmail.com>
2025-10-21 00:51:51 +0900

    mod: comment out webServer configuration in Playwright config

--- src/frontend/todos-web/playwright.config.ts
+++ src/frontend/todos-web/playwright.config.ts
@@ -75,9 +75,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: process.env.CI ? "npm run build && npm run start" : "npm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
