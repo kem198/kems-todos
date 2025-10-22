e57fa487c98fc923f87e7073a98748fc316fb006
kem198 <kenkenpa198@gmail.com>
2025-10-22 23:15:37 +0900

    fix: uncomment TodosNavigation and improve layout for adding todos

--- src/frontend/todos-web/src/features/todos/components/todos-client.tsx
+++ src/frontend/todos-web/src/features/todos/components/todos-client.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { JsonDisplay } from "@/components/display/json-display";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { ItemGroup } from "@/components/ui/item";
 import { Separator } from "@/components/ui/separator";
 import { TodoItem } from "@/features/todos/components/todo-item";
-import { TodosNavigation } from "@/features/todos/components/todos-navigation";
 import { ApiResponseData } from "@/types/common/api-response-data";
 import { Todo } from "@/types/todo/todo";
+import { PlusIcon } from "lucide-react";
 
 type TodosClientProps = {
   initialTodos: Todo[];
@@ -16,7 +18,14 @@ type TodosClientProps = {
 export function TodosClient({ initialTodos, responseData }: TodosClientProps) {
   return (
     <div className="flex w-full flex-col gap-4">
-      <TodosNavigation />
+      {/* <TodosNavigation /> */}
+      <div className="flex gap-2">
+        <Input type="text" placeholder="Type todo title..." />
+        <Button type="submit">
+          <PlusIcon />
+          Add
+        </Button>
+      </div>
       <ItemGroup className="gap-4">
         {initialTodos.map((todo) => (
           <TodoItem key={todo.todoId} todo={todo} />
