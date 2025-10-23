5d2b8974c71255d5946bfa405e5d4099a3abbce4
kem198 <kenkenpa198@gmail.com>
2025-10-23 19:31:00 +0900

    feat: add SerializedResponse type for improved response handling

--- src/frontend/todos-web/src/types/common/response.ts
+++ src/frontend/todos-web/src/types/common/response.ts
@@ -1,11 +1,11 @@
-export type ApiResponseData = {
+export type SerializedResponse = {
+  headers: Record<string, string>;
+  ok: boolean;
+  redirected: boolean;
   status: number;
   statusText: string;
-  ok: boolean;
   type: string;
-  redirected: boolean;
   url: string;
-  headers: Record<string, string>;
   body: string;
   bodyUsed: boolean;
 };
